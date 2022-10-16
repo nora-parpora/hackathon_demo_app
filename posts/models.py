36564b2d48463d9542029ffdb0b075b5c0455437
nora-parpora <noraparpora@gmail.com>
2022-10-16 12:35:52 +0300

    Adding rent_advert model

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from accounts.models import Employer
-from posts.enums import EmploymentType
+from accounts.models import Employer, HouseOwner
+from posts.enums import EmploymentType, RentalType
 
 
 class JobSector(models.Model):
@@ -14,9 +14,24 @@ class JobAdvert(models.Model):
     CITY_MAX_LENGTH = 50
     DESCR_MAX_LENGTH = 1000
     position = models.CharField(max_length=POSITION_MAX_LENGTH)
-    # ToDo Implement 'city' as a separate model and use OneToMany rel
+    # ToDo: Implement 'city' as a separate model and use OneToMany rel so new cities can be registered
     city = models.CharField(max_length=CITY_MAX_LENGTH)
     sector = models.ForeignKey(JobSector, blank=True, null=True, on_delete=models.SET_NULL)
     employment_type = models.CharField(max_length=30, choices=[(t.name, t.value) for t in EmploymentType])
     description = models.TextField(max_length=DESCR_MAX_LENGTH)
     employer = models.ForeignKey(Employer, on_delete=models.CASCADE)
+
+
+class RentAdvert(models.Model):
+    POSITION_MAX_LENGTH = 50
+    CITY_MAX_LENGTH = 50
+    DESCR_MAX_LENGTH = 1000
+
+    rental_type = models.CharField(max_length=30, choices=[(t.name, t.value) for t in RentalType], null=True,
+        blank=True, default='Rent')
+    # ToDo: Implement 'city' as a separate model and use OneToMany rel so new cities can be registered
+    city = models.CharField(max_length=CITY_MAX_LENGTH)
+    rooms = models.PositiveIntegerField()
+    rent = models.PositiveIntegerField()
+    description = models.TextField(max_length=DESCR_MAX_LENGTH)
+    owner = models.ForeignKey(HouseOwner, on_delete=models.CASCADE)
--- posts/urls.py
+++ posts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('jobs/', JobsView.as_view(), name="jobs_view"),
     path('jobs/search/', JobSearchView.as_view(), name="job_search"),
     path('jobs/<int:pk>/', JobView.as_view(), name="job"),
+
     ]
